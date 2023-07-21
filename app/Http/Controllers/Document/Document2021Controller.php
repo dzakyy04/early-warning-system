bb70b96d8a0f25fdc2a60b7e462077e19cfc0061
imancrrzii <imancarrazi777@gmail.com>
2023-07-21 09:05:40 +0700

    Create view for Add new dokumen 2021

--- app/Http/Controllers/Document/Document2021Controller.php
+++ app/Http/Controllers/Document/Document2021Controller.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Document;
 
 use App\Http\Controllers\Controller;
+use App\Models\Conceptor;
 use App\Models\Document\Document2021;
 use Illuminate\Http\Request;
 
@@ -17,7 +18,8 @@ class Document2021Controller extends Controller
 
     public function create()
     {
+        $conceptors = Conceptor::get();
         $title = 'Tambah Dokumen';
-        return view('dokumen.dokumen_2021.create', compact('title'));
+        return view('dokumen.dokumen_2021.create', compact('title', 'conceptors'));
     }
 }
