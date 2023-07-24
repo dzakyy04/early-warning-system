d217a0cd88b8e3c718c9ff9f0dfbcb2bd7a639c4
dzakyy04 <dzakylinggau@gmail.com>
2023-07-24 08:59:03 +0700

    Create a relationship between the conceptor and Document2020 & Document2021

--- app/Models/Conceptor.php
+++ app/Models/Conceptor.php
@@ -2,12 +2,24 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Document\Document2020;
+use App\Models\Document\Document2021;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Conceptor extends Model
 {
     use HasFactory;
 
     protected $guarded = ['id'];
+
+    public function document2020s()
+    {
+        return $this->hasMany(Document2020::class);
+    }
+
+    public function document2021s()
+    {
+        return $this->hasMany(Document2021::class);
+    }
 }
--- app/Models/Document/Document2021.php
+++ app/Models/Document/Document2021.php
@@ -2,12 +2,18 @@
 
 namespace App\Models\Document;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Conceptor;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Document2021 extends Model
 {
     use HasFactory;
 
     protected $guarded = ['id'];
+
+    public function conceptor()
+    {
+        return $this->belongsTo(Conceptor::class);
+    }
 }
